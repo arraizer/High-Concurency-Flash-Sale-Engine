fe9886a127775060e34ffcad364f95cc3e64a60c
arraizer <avoidme2809pls@gmail.com>
2026-08-02 17:18:35 +0700

    Modify by adding two more endpoint to test read, decrease stock in redis

--- flashsale-backend/src/main/java/com/example/flashsale/controller/FlashSaleController.java
+++ flashsale-backend/src/main/java/com/example/flashsale/controller/FlashSaleController.java
@@ -3,6 +3,8 @@ package com.example.flashsale.controller;
 import com.example.flashsale.dto.FlashSaleEventResponseDTO;
 import com.example.flashsale.dto.FlashSaleItemResponseDTO;
 import com.example.flashsale.service.FlashSaleService;
+import com.example.flashsale.service.FlashSaleStockService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api/flash-sales")
 @RequiredArgsConstructor
 public class FlashSaleController {
-
+    private final FlashSaleStockService stockService;
     private final FlashSaleService flashSaleService;
 
     // API: Lấy danh sách tất cả các đợt Flash Sale
@@ -27,4 +29,22 @@ public class FlashSaleController {
     public ResponseEntity<List<FlashSaleItemResponseDTO>> getEventItems(@PathVariable Long eventId) {
         return ResponseEntity.ok(flashSaleService.getItemsByEventId(eventId));
     }
+
+    @GetMapping("/items/{itemId}/redis-stock")
+    public ResponseEntity<Long> getRedisStock(@PathVariable Long itemId) {
+        return ResponseEntity.ok(stockService.getStockFromRedis(itemId));
+    }
+
+    @GetMapping("/items/{itemId}/deduct-test")
+    public ResponseEntity<String> deductStockTest(@PathVariable Long itemId,
+            @RequestParam(defaultValue = "1") int quantity) {
+        boolean success = stockService.deductStock(itemId, quantity);
+        if (success) {
+            return ResponseEntity
+                    .ok("Trừ kho Redis thành công! Stock còn lại: " + stockService.getStockFromRedis(itemId));
+        } else {
+            return ResponseEntity.badRequest().body("Trừ kho thất bại: Sản phẩm đã HẾT HÀNG!");
+        }
+    }
+
 }
